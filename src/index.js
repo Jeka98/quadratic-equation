aa94f26d9b928e3b93e20a39b2fad9aac025f575
Jeka98 <Zhukjeka1998@yandex.by>
2018-09-19 22:29:05 +0300

    finished solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+
+  let a = equation.split('*')[0].replace(/\s/g, '');
+  let b = equation.split('*')[1].replace('x^2','').replace(/\s/g, '');
+  let c = equation.split('*')[2].replace('x','').replace(/\s/g, '');
+
+  let D = b * b - 4 * a * c;
+
+  if(D > 0){
+    let x1 = Math.round((-b - Math.sqrt(D)) / (2*a));
+    let x2 = Math.round((-b + Math.sqrt(D)) / (2*a));
+    let result = [x2,x1];
+    return result.sort((x,y) => (x-y));
+  }
+  else if(D == 0){
+    return -b /2 * a;
+  }
+  else{return null;}
 }
